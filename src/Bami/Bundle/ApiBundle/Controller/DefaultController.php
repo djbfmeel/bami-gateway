a5ae9dea6df9ec09370d4b29a762a7e9ed8bfd08
djbfmeel <vanmeeldennis@msn.com>
2017-01-09 18:55:54 +0100

    Callback test added

--- src/Bami/Bundle/ApiBundle/Controller/DefaultController.php
+++ src/Bami/Bundle/ApiBundle/Controller/DefaultController.php
@@ -2,15 +2,40 @@
 
 namespace Bami\Bundle\ApiBundle\Controller;
 
+use Symfony\Bridge\Monolog\Logger;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController
 {
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @return JsonResponse
      */
     public function indexAction()
     {
+        return new JsonResponse();
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function callbackAction(Request $request)
+    {
+        $data = $request->request->all();
+
+        $this->logger->addDebug(json_encode($data));
+
         return new JsonResponse();
     }
 }
